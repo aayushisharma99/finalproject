7313e33b992b85d1bb3a1c3e3d0b3cf726902f13
aayushisharma99 <65369427+aayushisharma99@users.noreply.github.com>
2021-10-14 17:23:27 +0530

    Add files via upload

---
+++ config.js
@@ -0,0 +1,8 @@
+module.exports = {
+    'secretKey': '12345-67890-09876-54321',
+    'mongoUrl' : 'mongodb://localhost:27017/confusion',//used to be conFusion changed to fusion
+    'facebook': {
+        clientId: '389542755511992',
+        clientSecret: '38204c2483fc7d52888094da771e1481'
+    }
+}
